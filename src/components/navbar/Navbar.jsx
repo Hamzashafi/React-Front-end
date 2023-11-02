d899522aa5ad77b8ca7259c5da60f9a2b07c60ba
Hamza_Shafi <hamzashafi946@gmail.com>
2023-11-02 13:44:15 +0500

    New lines

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.png';
 import './navbar.css';
 
-const Navbar = () => 
- (
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+ return (
     <div className="gpt3__navbar">
     <div className="gpt3__navbar-links">
       <div className="gpt3__navbar-links_logo">
+          
         <img src={logo} alt='logo' />
       </div>
       </div>
@@ -17,8 +20,20 @@ const Navbar = () =>
         <button type="button">SIGN UP</button>
         &nbsp;&nbsp;&nbsp;
       </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+           {toggleMenu && (
+        <div className="gpt3__navbar-menu_container scale-up-center">
+        <div className="gpt3__navbar-menu_container-links-sign">
+            <button type ='button'>LOGIN</button>
+            <button type="button">SIGN UP</button>
+          </div>
+        </div>
+        )}
+      </div>
       </div>
   );
-
-
+}
 export default Navbar;
--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Untitled from '../../assets/Untitled-1.png';
 import './header.css';
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+const Header = () =>  (
+    <div className="gpt3__header-image">
+      <img src={Untitled-1} />
+      <div className='gpt3__header-content'>
+      <h1 ><b>Unlock more <br /> revenue through <br /> streaming</b></h1>
+      </div>
+      <p>Whether you're a content creator, a brand, or a small business
+      - Uchlo can help <br />supercharge your revenues. Sign up and see for yourself how you can 10x your <br />conversion rates!</p>
+      <div className='gpt3__header-content__input'>
+      &nbsp;&nbsp;&nbsp;<button type="button">Get Started</button>
+      </div>
+    </div>
+
+   
+  );
 
 export default Header;
