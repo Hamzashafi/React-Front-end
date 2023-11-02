75a9e8cf95c7229b1f7f73bb10cb778a1c27899d
Hamza_Shafi <hamzashafi946@gmail.com>
2023-11-02 15:15:47 +0500

    new line

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -4,9 +4,9 @@ import './header.css';
 
 const Header = () =>  (
     <div className="gpt3__header-image">
-      <img src={Untitled-1} />
+      <img src='Untitled' />
       <div className='gpt3__header-content'>
-      <h1 ><b>Unlock more <br /> revenue through <br /> streaming</b></h1>
+      <h1><b>Unlock more <br /> revenue through <br /> streaming</b></h1>
       </div>
       <p>Whether you're a content creator, a brand, or a small business
       - Uchlo can help <br />supercharge your revenues. Sign up and see for yourself how you can 10x your <br />conversion rates!</p>
