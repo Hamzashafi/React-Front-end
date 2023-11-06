71837fac0c5397ccbb6c4cb2a798db9198b32ccb
Hamza_Shafi <hamzashafi946@gmail.com>
2023-11-06 13:02:45 +0500

    new line

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { image1, image2, image3 } from './import';
 import './brand.css';
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='brand section__padding'>
+      <div>
+        <img src={image1} />
+        <h4>Live Streaming</h4>
+        <p>Stream hassle-free to your socials instantly.</p><br />
+      </div>
+      <div>
+        <img src={image2} />
+        <h4>Customizable Product Display</h4>
+        <p>Make your products look irresistible.</p><br />
+      </div>
+      <div>
+        <img src={image3} />
+        <h4>Audience Engagement Tools</h4>
+        <p>Leverage the power of 2-way interactions.</p><br />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Brand/import.js
@@ -0,0 +1,9 @@
+import image1 from '../../assets/Group 645.png';
+import image2 from '../../assets/Group 649.png';
+import image3 from '../../assets/Group 647.png';
+
+export{
+    image1,
+    image2,
+    image3
+};
